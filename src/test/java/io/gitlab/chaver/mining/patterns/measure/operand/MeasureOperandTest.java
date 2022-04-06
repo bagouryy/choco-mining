e25db1e77d343b4117b6beef29b9f7eca7f6398a
Charles Vernerey <charlesvernerey2@gmail.com>
2022-04-06 16:40:01 +0200

    Simplify MeasureOperand tests

--- src/test/java/io/gitlab/chaver/mining/patterns/measure/operand/MeasureOperandTest.java
+++ src/test/java/io/gitlab/chaver/mining/patterns/measure/operand/MeasureOperandTest.java
@@ -1,46 +1,46 @@
 package io.gitlab.chaver.mining.patterns.measure.operand;
 
 import io.gitlab.chaver.mining.patterns.measure.Measure;
-import io.gitlab.chaver.mining.patterns.measure.MeasureFactory;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
+import static io.gitlab.chaver.mining.patterns.measure.MeasureFactory.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MeasureOperandTest {
 
     @Test
     public void testConvertArea() {
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.freq())), MeasureFactory.area().maxConvert());
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.length())), MeasureFactory.area().minConvert());
+        assertEquals(new HashSet<>(Arrays.asList(freq())), area().maxConvert());
+        assertEquals(new HashSet<>(Arrays.asList(length())), area().minConvert());
     }
 
     @Test
     public void testConvertFreqAreaGr() {
-        List<Measure> mList = Arrays.asList(MeasureFactory.freq(), MeasureFactory.area(), MeasureFactory.growthRate());
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.freq(), MeasureFactory.freq1())), MeasureOperand.maxConvert(mList));
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.length(), MeasureFactory.freq2())), MeasureOperand.minConvert(mList));
+        List<Measure> mList = Arrays.asList(freq(), area(), growthRate());
+        assertEquals(new HashSet<>(Arrays.asList(freq(), freq1())), MeasureOperand.maxConvert(mList));
+        assertEquals(new HashSet<>(Arrays.asList(length(), freq2())), MeasureOperand.minConvert(mList));
     }
 
     @Test
     public void testConvertMean() {
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.min(0))), MeasureFactory.mean(0).maxConvert());
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.max(0))), MeasureFactory.mean(0).minConvert());
+        assertEquals(new HashSet<>(Arrays.asList(min(0))), mean(0).maxConvert());
+        assertEquals(new HashSet<>(Arrays.asList(max(0))), mean(0).minConvert());
     }
 
     @Test
     public void testConvertAllConf() {
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.freq(), MeasureFactory.maxFreq())), MeasureFactory.allConf().maxConvert());
-        assertEquals(new HashSet<>(Arrays.asList()), MeasureFactory.allConf().minConvert());
+        assertEquals(new HashSet<>(Arrays.asList(freq(), maxFreq())), allConf().maxConvert());
+        assertEquals(new HashSet<>(Arrays.asList()), allConf().minConvert());
     }
 
     @Test
     public void testConvertAnyConf() {
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.freq())), MeasureFactory.anyConf().maxConvert());
-        assertEquals(new HashSet<>(Arrays.asList(MeasureFactory.minFreq())), MeasureFactory.anyConf().minConvert());
+        assertEquals(new HashSet<>(Arrays.asList(freq())), anyConf().maxConvert());
+        assertEquals(new HashSet<>(Arrays.asList(minFreq())), anyConf().minConvert());
     }
 
 }
