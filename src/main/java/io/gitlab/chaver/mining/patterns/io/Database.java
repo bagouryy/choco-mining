d2e76b1fd3256b7cb1ce41a7c6ed845373afc584
Charles Vernerey <charlesvernerey2@gmail.com>
2022-04-23 01:10:36 +0200

    Update LICENSE

--- src/main/java/io/gitlab/chaver/mining/patterns/io/Database.java
+++ src/main/java/io/gitlab/chaver/mining/patterns/io/Database.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of io.gitlab.chaver:data-mining
+ *
+ * Copyright (c) 2022, IMT Atlantique
+ *
+ * Licensed under the MIT license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
 package io.gitlab.chaver.mining.patterns.io;
 
 import java.util.Arrays;
