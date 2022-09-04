899792ac9ae406d6cc3540f6715694830ac53170
Charles Vernerey <charlesvernerey2@gmail.com>
2022-09-04 15:42:47 +0200

    Update license

--- src/main/java/io/gitlab/chaver/mining/examples/ExampleAdequateClosure.java
+++ src/main/java/io/gitlab/chaver/mining/examples/ExampleAdequateClosure.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of io.gitlab.chaver:data-mining (https://gitlab.com/chaver/data-mining)
+ *
+ * Copyright (c) 2022, IMT Atlantique
+ *
+ * Licensed under the MIT license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
 package io.gitlab.chaver.mining.examples;
 
 import io.gitlab.chaver.mining.patterns.constraints.AdequateClosureDC;
